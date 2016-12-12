3632a27fafd5c6b4e31e1c63cf734231fbf7f4df
coenvc <coenvc@gmail.com>
2016-12-12 09:55:57 +0100

    Commit voor het verhelpen van de merge errors

---
+++ Eyect4RailsWebApp/Exceptions/CreateObjectException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Eyect4RailsWebApp.Exceptions
+{
+    public class CreateObjectException:Exception
+    {
+    }
+}
