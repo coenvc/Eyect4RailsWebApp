a417fd95764ffe8e97b3fd58ee79df946f309d71
Reinoud van Zoelen <354261@student.fontys.nl>
2016-12-14 00:49:58 +0100

    First draft of the mssql remise repository

---
+++ Eyect4RailsWebApp.Tests/MSSQLRepositoryTest/TU_MSSQLRemiseRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Eyect4RailsWebApp.Tests.MSSQLRepositoryTest
+{
+    [TestClass]
+    public class TU_MSSQLRemiseRepository
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
