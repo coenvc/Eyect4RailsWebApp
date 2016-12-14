03f024709c7b1b3cf6891000261879779be8d5e6
Reinoud van Zoelen <354261@student.fontys.nl>
2016-12-14 12:19:33 +0100

    Completed the CreateObjectFromReader part of the mssqlmaintenancerepository

--- Eyect4RailsWebApp.Tests/MSSQLRepositoryTest/TU_MSSQLMaintenanceRepository.cs
+++ Eyect4RailsWebApp.Tests/MSSQLRepositoryTest/TU_MSSQLMaintenanceRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using eyect4rails.Classes;
+using Eyect4RailsWebApp.Context;
 using Eyect4RailsWebApp.Repositories.MSSQLRepository;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -13,19 +14,23 @@ namespace Eyect4RailsWebApp.Tests.MSSQLRepositoryTest
         public void tu_MSSQLMaintenance_CreateObjectFromReader()
         {
             // arrange
-            MSSQLMaintenanceRepository Repository = new MSSQLMaintenanceRepository();
-            Maintenance maintenance;
+            MaintenanceContext context = new MaintenanceContext(new MSSQLMaintenanceRepository());
+            Maintenance maintenance0;
+            Maintenance maintenance1;
 
             //act
-            maintenance = Repository.GetAll()[0];
+            maintenance0 = context.GetById(0);
+            maintenance1 = context.GetById(1);
 
             //assert
-            Assert.AreEqual(1, maintenance.Employee.Id, "Employee is not picked up correctly");
-            Assert.AreEqual(1, maintenance.Tram.Id, "Tram is not picked up correctly");
-            Assert.AreEqual(new DateTime(2016, 12, 12, 08, 00, 00), maintenance.ScheduledDate);
-            Assert.AreEqual(new DateTime(2016, 12, 12, 12, 00, 00), maintenance.AvailableDate);
-            Assert.AreEqual(1, maintenance.TaskID);
-            Assert.AreEqual("Kleine Schoonmaak", maintenance.Task, "Task description is not picked up correctly");
+            Assert.AreEqual(1, maintenance0.Employee.Id, "Employee is not picked up correctly");
+            Assert.AreEqual(1, maintenance0.Tram.Id, "Tram is not picked up correctly");
+            Assert.AreEqual(new DateTime(2016, 12, 12, 08, 00, 00), maintenance0.ScheduledDate);
+            Assert.AreEqual(new DateTime(2016, 12, 12, 12, 00, 00), maintenance0.AvailableDate);
+            Assert.AreEqual(1, maintenance0.TaskID);
+            Assert.AreEqual("Kleine Schoonmaak", maintenance0.Task, "Task description is not picked up correctly");
+
+            Assert.AreEqual(DateTime.MaxValue, maintenance1.AvailableDate);
         }
 
         [TestMethod]
