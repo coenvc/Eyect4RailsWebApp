a27ea24a082c0167580993bd33990cc7fcb8b9ca
Reinoud van Zoelen <354261@student.fontys.nl>
2016-12-14 19:13:41 +0100

    Commented some items in IO/Delete and IO/List because they needed items that did not yet exist.

--- Eyect4RailsWebApp/Logic/IOLogic.cs
+++ Eyect4RailsWebApp/Logic/IOLogic.cs
@@ -42,5 +42,10 @@ namespace Eyect4RailsWebApp.Logic
         {
             return Tram.GetBySectorId(id);
         }
+
+        public List<Tram> GetByRemiseId(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
