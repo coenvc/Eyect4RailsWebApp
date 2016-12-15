7d4948d6ecdf9ffa5f63d744fa8af6f028d9a797
Reinoud van Zoelen <354261@student.fontys.nl>
2016-12-15 13:42:29 +0100

    Created and finished the MSSQLRemiseRepository

--- Eyect4RailsWebApp/Models/Remise.cs
+++ Eyect4RailsWebApp/Models/Remise.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +12,17 @@ namespace eyect4rails.Classes
     public class Remise:ICruddable
     {
         #region Properties
+        [ScaffoldColumn(false)]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "A name for the Remise is required."),
+            DisplayName("Remise name")]
         public string Name { get; set; }
+        
+        [DisplayName("Tracks on the Remise")]
         public List<Track> Tracklist { get; set; }
+
+        [DisplayName("Trams in the Remise")]
         public List<Tram> Tramlist { get; set; }
         #endregion
 
