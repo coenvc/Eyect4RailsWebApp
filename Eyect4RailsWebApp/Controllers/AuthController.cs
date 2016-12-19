558ec1173920c1c841f112e526ae7d080dc51dfc
coenvc <coenvc@gmail.com>
2016-12-19 09:50:57 +0100

    Authenticatie

---
+++ Eyect4RailsWebApp/Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using eyect4rails.Classes;
+using Eyect4RailsWebApp.Logic;
+
+namespace Eyect4RailsWebApp.Controllers
+{
+    public class AuthController : Controller
+    {
+        private EmployeeLogic Logic = new EmployeeLogic();
+        // GET: Auth 
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Employee employee)
+        {
+            string username = employee.Username;
+            string password = employee.Password;
+            try
+            {
+                Logic.GetByUsernamePassword(username, password);
+                Session["LoggedInEmployee"] = employee;
+                return RedirectToAction("Index", "Employee");  
+            }
+            catch (Exception ex)
+            {
+                return View();
+            }
+        } 
+       
+
+        
+
+       
+    }
+}
