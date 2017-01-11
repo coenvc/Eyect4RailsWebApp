ef3d82b9fa097f3f257e7636542e2afa4f9c16d6
coenvc <coenvc@gmail.com>
2017-01-11 13:35:40 +0100

    WebApi Controller?

---
+++ Eyect4RailsWebApp/Controllers/RemiseApiController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Eyect4RailsWebApp.Logic;
+using Eyect4RailsWebApp.Repositories.MSSQLRepository;
+
+namespace Eyect4RailsWebApp.Controllers
+{
+    public class RemiseApiController : ApiController
+    {
+        RemiseLogic logic = new RemiseLogic(new MSSQLRemiseRepository());
+
+        public IHttpActionResult GetSector(int id)
+        {
+            var sector = logic.GetById(id); 
+            if (sector == null)
+            {
+                return NotFound();
+            }
+            return Ok(sector);
+        }
+    }
+}
